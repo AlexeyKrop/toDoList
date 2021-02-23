3f3c000923f9871654a099398cc4ed179f8ce49d
Aleksey Kropotin <kropotin_aleksey@mail.ru>
2021-02-23 13:38:28 +0300

    переделал

--- script.js
+++ script.js
@@ -49,76 +49,16 @@ let addToDo = function(){
 
 todoControl. addEventListener('submit', function(event){
     event.preventDefault();
-   
-    let newToDo = {
+    if(headerInput.value.trim() !== ''){
+        let newToDo = {
         value: headerInput.value,
         completed: false,
         };
-    toDoArr.push(newToDo);
-
+        toDoArr.push(newToDo);
+    }
     addToDo();
 });
 
-//  let todoControl = document.querySelector('.todo-control');
-// let headerButton = document.querySelector('.header-button');
-// let headerInput = document.querySelector('.header-input');
-// let list = document.querySelector('.todo-list');
-// let listComleted = document.querySelector('.todo-completed');
-// let todoItem = list.querySelectorAll('.todo-item');
-// let toDoArr = ['Сварить кофе', 'Помыть посуду'];
-
-
-// // функция возвращает новый элемент
-// function createElement(evt){
-//     evt.preventDefault();
-//     let newElement = document.createElement('li');
-//     newElement.classList.add('todo-item');
-//     newElement.textContent = headerInput.value;
-//     if (newElement.textContent.trim() !== '') {
-//      newElement.innerHTML += '<div class="todo-buttons">' +
-// 				'<button class="todo-remove"></button>' +
-// 				'<button class="todo-complete"></button>' +
-// 			    '</div>';
-//     headerInput.value = '';
-//     list.append(newElement);
-
-//     toDoArr.push(newElement.outerText);   
-   
-// }
-//     let todoItem = list.querySelectorAll('.todo-item');
-//     let todoComleteBtn = list.querySelectorAll('.todo-complete');
-//     for(let i = 0; i < todoItem.length; i++){
-//         todoComleteBtn[i].addEventListener('click', function(){
-//             listComleted.append(todoItem[i]);
-//         });
-//     }
-    
-// }
-
-// todoControl.addEventListener('submit', createElement);
-
-// let todoRemoveBtn = document.querySelectorAll('.todo-remove');
-
-
-// for(let i = 0; i < todoItem.length; i++){
-//     todoRemoveBtn[i].addEventListener('click', function(){
-//         todoItem[i].parentNode.removeChild(todoItem[i]);
-//     });
-    
-// }
-// for(let i = 0; i < todoItem.length; i++){
-//     todoRemoveBtn[i].addEventListener('click', function(){
-//         todoItem[i].parentNode.removeChild(todoItem[i]);
-//     });
-    
-// }
-
-
-
-
-
-
-    
 
 
 
