56e7c540b6f5e9d48c99ef808baedf5a3bbbd14f
AlexeyKrop <65413959+AlexeyKrop@users.noreply.github.com>
2021-03-17 13:03:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,67 @@
 'use strict';
 
-let todoControl = document.querySelector('.todo-control'),
-    todoList = document.querySelector('.todo-list'),
-    headerInput = document.querySelector('.header-input'),
-    todoCompleted = document.querySelector('.todo-completed');
-
-let toDoArr = [
- 
-];  
-
-let addToDo = function(){
-    if (localStorage.getItem('value')){
-        toDoArr = JSON.parse(localStorage.getItem('value'));
-    }
-    todoList.textContent = '';
-    todoCompleted.textContent = '';
-    headerInput.value = '';
-    toDoArr.forEach(function(item, index){
-        let li = document.createElement('li');
-        li.classList.add('todo-item');
-        li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
-                       '<div class="todo-buttons">' +
-                            '<button class="todo-remove"></button>' +
-                            '<button class="todo-complete"></button>' +
-                       '</div>';
-        if(item.completed){
-          todoCompleted.append(li);  
-        } else{
-            todoList.append(li);
-        }  
-
-        let toDoCompleteBtn = li.querySelector('.todo-complete');
-        toDoCompleteBtn.addEventListener('click', function(){
-            item.completed = !item.completed;
-             if(item.completed){
-          todoCompleted.append(li);
-          localStorage.setItem('value', JSON.stringify(toDoArr)); 
-        } else{
-            todoList.append(li);
-            localStorage.setItem('value', JSON.stringify(toDoArr)); 
-        }  
+function Todo(){
+    this.todoControl = document.querySelector('.todo-control');
+    this.todoList = document.querySelector('.todo-list');
+    this.headerInput = document.querySelector('.header-input');
+    this.todoCompleted = document.querySelector('.todo-completed');
+    this.toDoArr = [];  
+    this.addToDo = function(){
+        if (localStorage.getItem('value')){
+            this.toDoArr = JSON.parse(localStorage.getItem('value'));
+        }
+        this.todoList.textContent = '';
+        this.todoCompleted.textContent = '';
+        this.headerInput.value = '';
+        this.toDoArr.forEach((item, index) => {
+            let li = document.createElement('li');
+            li.classList.add('todo-item');
+            li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+                           '<div class="todo-buttons">' +
+                                '<button class="todo-remove"></button>' +
+                                '<button class="todo-complete"></button>' +
+                           '</div>';
+            if(item.completed){
+              this.todoCompleted.append(li);  
+            } else{
+                this.todoList.append(li);
+            }  
+    
+            let toDoCompleteBtn = li.querySelector('.todo-complete');
+            toDoCompleteBtn.addEventListener('click', () => {
+                item.completed = !item.completed;
+                 if(item.completed){
+              this.todoCompleted.append(li);
+              localStorage.setItem('value', JSON.stringify(this.toDoArr)); 
+            } else{
+                this.todoList.append(li);
+                localStorage.setItem('value', JSON.stringify(this.toDoArr)); 
+            }  
+            });
+    
+            let todoRemoveBtn = li.querySelector('.todo-remove');
+            todoRemoveBtn.addEventListener('click', () => {
+                this.toDoArr.splice(index, 1);
+                localStorage.setItem('value', JSON.stringify(this.toDoArr)); 
+                this.addToDo();
+            });
         });
-
-        let todoRemoveBtn = li.querySelector('.todo-remove');
-        todoRemoveBtn.addEventListener('click', function(){
-            toDoArr.splice(index, 1);
-            localStorage.setItem('value', JSON.stringify(toDoArr)); 
-            addToDo();
+    this.todoControl. addEventListener('submit', (event) => {
+            event.preventDefault();
+            if(this.headerInput.value.trim() !== ''){
+                let newToDo = {
+                value: this.headerInput.value,
+                completed: false,
+                };
+                this.toDoArr.push(newToDo);
+                localStorage.setItem('value', JSON.stringify(this.toDoArr));
+            }
+            this.addToDo();
         });
-    });
-};
-
-todoControl. addEventListener('submit', function(event){
-    event.preventDefault();
-    if(headerInput.value.trim() !== ''){
-        let newToDo = {
-        value: headerInput.value,
-        completed: false,
-        };
-        toDoArr.push(newToDo);
-        localStorage.setItem('value', JSON.stringify(toDoArr));
-    }
-    addToDo();
-});
-addToDo();
-
+    };
+}
+let toDo = new Todo();
+toDo.addToDo();
 
 
 
